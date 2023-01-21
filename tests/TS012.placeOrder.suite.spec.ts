f5ee8895eb50db852f0602eed6b1bd67ed91cfbf
abdelnaby1 <ahmedabdelnaby471@gmail.com>
2023-01-21 12:41:43 +0200

    complete place order suite

--- tests/TS012.placeOrder.suite.spec.ts
+++ tests/TS012.placeOrder.suite.spec.ts
@@ -52,6 +52,46 @@ test.describe('Place Order Suite',() =>{
         checkoutPage.payment.enterPaymentDetails(card);
         await expect(page.getByText("Your order has been placed successfully!")).toBeVisible();
     });
+    test('TC018 - it should register before checkout',async({page,prodcutsPage,homePage,registerPage,cartPage,checkoutPage}) =>{       
+        await expect(page).toHaveTitle("Automation Exercise");
+        await homePage.clickLoginLink();
+        await registerPage.signup("moh",`moh${Date.now()}@gmail.com`);
+        await registerPage.completeSignup(
+            "123456","26","8","1992","Moh","Abdelnaby",
+            "Sah","Cairo,Helwan","helwan elbald","Canada",
+            "Giza","el-Giza","49944","01099980000"
+            );
+        await expect(registerPage.accountMsg).toBeVisible();
+        await expect(registerPage.accountMsg).toContainText("Account Created!");
+        await registerPage.continue();
+        await expect(homePage.user).toContainText('Logged in as');
+        await homePage.clickProductsLink();
+        await prodcutsPage.addToCart(0);
+        await prodcutsPage.addToCart(1);
+        await homePage.clickCartLink();
+        expect(page.url()).toContain('/view_cart');
+        await cartPage.clickCheckout();
+        // await Promise.all([
+        //     expect(page.url()).toContain('/view_cart'),
+        //     expect(cartPage.checkoutModal).toBeVisible()
+        // ]);       
+        await Promise.all([
+            expect(checkoutPage.addressDetailsH).toBeVisible(),
+            expect(checkoutPage.reviewOrderH).toBeVisible(),
+        ]);
+        await checkoutPage.placeOrder("thank you");
+        let card = {
+            name:"john2",
+            num: "1231098745642309",
+            cvc:"124",
+            month:"11",
+            year:"24"
+        }
+
+        //don't await this function 
+        checkoutPage.payment.enterPaymentDetails(card);
+        await expect(page.getByText("Your order has been placed successfully!")).toBeVisible();
+    });
 
 
 });
